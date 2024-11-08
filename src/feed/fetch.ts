9434f1033db70560f205b10d92f89e29795824ae
tenjo-t <tenjo0314takashi@gmail.com>
2024-11-08 22:25:29 +0900

    initial commit

---
+++ src/feed/fetch.ts
@@ -0,0 +1,105 @@
+import type { SiteInfo } from "./list";
+
+import RSSParser from "rss-parser";
+
+export type Item = {
+	creator?: string;
+	title: string;
+	link: string;
+	pubDate: string;
+	contentSnippet?: string;
+};
+
+export type ItemForAll = {
+	site: string;
+} & Item;
+
+export type Feed = {
+	link: string;
+	title: string;
+	description?: string;
+	items: Item[];
+};
+
+const cache = new Map<string, Feed>();
+
+const rssParser = new RSSParser();
+
+export async function fetchFeed(
+	url: string,
+	siteName: string,
+	siteLink: string,
+): Promise<Feed | undefined> {
+	if (cache.has(url)) return cache.get(url);
+
+	const res = await fetch(url);
+	if (!res.ok) {
+		console.warn(`${siteName}のデータを取得できませんでした`);
+		return;
+	}
+
+	const parsed = await rssParser.parseString(await res.text());
+	const items = parsed.items.map((i) => {
+		i;
+		return {
+			creator: i.creator,
+			// biome-ignore lint/style/noNonNullAssertion: 無いと困る
+			title: i.title!,
+			// biome-ignore lint/style/noNonNullAssertion: 無いと困る
+			link: i.link!,
+			pubDate:
+				i.isoDate ?? new Date(i.pubDate ? i.pubDate : Date.now()).toISOString(),
+			contentSnippet: i.contentSnippet ?? i.summary,
+		};
+	});
+
+	const feed = {
+		link: parsed.link ?? siteLink,
+		title: parsed.title ?? siteName,
+		description: parsed.description,
+		items,
+	};
+
+	cache.set(url, feed);
+
+	return feed;
+}
+
+export async function collectFeeds(
+	list: SiteInfo[],
+): Promise<Record<string, Feed>> {
+	const feeds = await Promise.all(
+		list.map((site) => fetchFeed(site.rss, site.title, site.url)),
+	);
+
+	const result: Record<string, Feed> = {};
+	for (let i = 0; i < list.length; i++) {
+		const site = list[i];
+		const feed = feeds[i];
+
+		if (feed === undefined) continue;
+
+		result[site.param] = feed;
+	}
+
+	return result;
+}
+
+export async function collectAllItems(list: SiteInfo[]): Promise<ItemForAll[]> {
+	const feeds = await collectFeeds(list);
+	return Object.values(feeds)
+		.reduce<ItemForAll[]>(
+			(pre, cur) =>
+				pre.concat(
+					cur.items.map<ItemForAll>((i) => ({ site: cur.title, ...i })),
+				),
+			[],
+		)
+		.sort((a, b) => (a.pubDate < b.pubDate ? 1 : -1))
+		.reduce<ItemForAll[]>((pre, cur, i) => {
+			if (i === 0 || pre[pre.length - 1].link !== cur.link) {
+				pre.push(cur);
+			}
+			return pre;
+		}, []);
+}
---
+++ src/feed/list.ts
@@ -0,0 +1,55 @@
+export type SiteInfo = {
+	rss: string;
+	url: string;
+	title: string;
+	param: string;
+	logo: string;
+	creator?: string;
+};
+
+export const siteList = [
+	{
+		rss: "https://zenn.dev/feed",
+		url: "https://zenn.dev/",
+		title: "Zenn",
+		param: "zenn",
+		logo: "/logo/zenn.svg",
+	},
+	{
+		rss: "https://jser.info/rss/",
+		url: "https://jser.info/",
+		title: "JSer.info",
+		param: "jser_info",
+		logo: "/logo/jser_info.png",
+	},
+	{
+		rss: "https://gihyo.jp/feed/atom",
+		url: "https://gihyo.jp/",
+		title: "gihyo.jp",
+		param: "gihyo_jp",
+		logo: "",
+	},
+	{
+		rss: "https://zenn.dev/topics/rust/feed",
+		url: "https://zenn.dev/topics/rust",
+		title: "Zenn (Rust)",
+		param: "zenn_rust",
+		logo: "/logo/zenn.svg",
+	},
+	{
+		rss: "https://blog.jxck.io/feeds/atom.xml",
+		url: "https://blog.jxck.io/",
+		title: "Jxck's blog",
+		param: "blog_jxck",
+		logo: "",
+		creator: "Jxck",
+	},
+	{
+		rss: "https://azukiazusa.dev/rss.xml",
+		url: "https://azukiazusa.dev/",
+		title: "azukiazusaのテックブログ2",
+		param: "azukiazusa",
+		logo: "",
+		creator: "azukiazusa1",
+	},
+] satisfies SiteInfo[];
